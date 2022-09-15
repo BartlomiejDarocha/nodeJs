208d9960d318a524b2d88c8bf04c80b4d92fd9c0
BartekDarocha <bartlomiejdarocha@gmail.com>
2022-09-15 23:43:47 +0200

    Call bind apply example

--- JsExamples/call_bind_apply.js
+++ JsExamples/call_bind_apply.js
@@ -1,8 +1,33 @@
 const humanA = {
-  name: 'Tom',
+  name: 'Eric',
   sayMyName: function() {
     console.log(`My name is ${this.name}`);
   }
 }
 
-humanA.sayMyName();
+const humanB = {
+  name: 'Hyde',
+}
+
+const humanC = {
+  name: 'Celso',
+  humnaCSaySomeThing: function(text) {
+    console.log(`${this.name} says ${text}`);
+  }
+}
+
+const printlogData = function() {
+  console.log('humanA', humanA);
+  console.log('humanB', humanB);
+  console.log('humanC', humanC);
+  console.log('Display CALL funcions change context on Human A for Human B');
+  humanA.sayMyName.call(humanB);
+  console.log('Display APPLY function change context on Human C for Human B');
+  humanC.humnaCSaySomeThing.apply(humanB, ['What ever']);
+  console.log('Display BIND function change context on Human C for Human B');
+  const bindedReadytoExecute = humanC.humnaCSaySomeThing.bind(humanB, 'You have to excecute me! and What ever');
+  bindedReadytoExecute();
+}
+
+printlogData();
+
